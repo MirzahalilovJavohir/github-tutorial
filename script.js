5c8d6f79b329770b517ab35b331f9ec4b3944bd7
Javohir6854 <exaple@gmail.com>
2024-01-12 19:34:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,145 @@
+"use strict"; 
+
+// const person = {
+//     name: "Javohir"
+// }
+// const clientName = "Javohir "; 
+// person.name = "Mirzaxalilov"
+
+// console.log(clientName + person.name);
+
+
+
+
+// Number 
+let number = 5.6; //Data type = number
+console.log(8 / 0); // Infinity 
+console.log("Javohir" * 8); // Nan
+
+
+
+// String
+
+// const clientName = "Javohir"; 
+
+
+
+
+// Boolean 
+
+// const isMarried = false; 
+// const isCircleEarth = true;
+
+// true = xa  (To'g'ri)
+// false = yoq (notog'ri)
+
+
+
+
+
+// // null
+// console.log(clientAge);
+
+
+
+
+
+// //undefined
+// let und;
+// console.log(und);
+
+
+
+
+
+//object 
+
+const thief = {
+    //key: value
+    age: 20,  // number
+    Jacket: "black", //string
+    isLong:false, // boolean
+};
+// console.log(thief.Jacket);
+
+
+
+
+
+// Array 
+
+// const colors = ["red", "black", "green", 10, [], {}];
+// console.log(colors[0]);
+
+// const place = confirm("Are you here?");
+// console.log(place);
+
+// const currentPlace = prompt("Where are you?", "Toshkentda");
+// console.log(currentPlace);
+
+// const age = +prompt("How old are you ?");
+// console.log(typeof age);
+
+// const favoriteColor = [];
+
+// favoriteColor [0] = prompt("What's your favorite color #1");
+// favoriteColor [1] = prompt("What's your favorite color #2");
+// favoriteColor [2] = prompt("What's your favorite color #3");
+
+// console.log(favoriteColor);
+
+// const channel = prompt("What's your favorite instagram");
+// console.log(`https://youtube.com/${channel}`);
+
+// const user = "Javoxir";
+// const job = "Web devoloper"; 
+
+// console.log(`User name is ${user}, he is ${job}`);
+
+
+
+
+
+
+
+// incr, decr
+
+// let incr = 10;
+// let decr = 10; 
+
+// console.log(incr++); // prefiks
+// console.log(decr--); // prefiks
+
+
+
+
+
+// Prosent 
+
+// console.log(20 % 3); 
+
+
+
+
+// tenglashtirish = "==" va "==="
+
+// console.log(5*5 === "25"); //false
+// console.log(5*5 == "25");  //true
+
+
+
+
+// && = va || = yoki
+
+// const isAge = true; 
+// const isClose = true;
+// const isChecked = false; 
+
+// console.log(isAge || isClose && isChecked);  // true qiymat qaytaradi
+// console.log(isAge && isClose && isChecked);  // False qiymat qaytaradi
+// console.log(isAge && isClose && !isChecked); // true qiymat qaytaradi
+
+// console.log( 2+2 * 2 == 6);
+
+
+
